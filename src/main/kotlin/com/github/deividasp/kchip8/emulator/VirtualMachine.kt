d501a883ff7ff06886eb7df622ce53be7e068afe
Deividas Popelskis <deividas.popelskis@gmail.com>
2017-10-10 22:28:08 +0300

    Added virtual machine base

---
+++ src/main/kotlin/com/github/deividasp/kchip8/emulator/VirtualMachine.kt
@@ -0,0 +1,37 @@
+package com.github.deividasp.kchip8.emulator
+
+import java.nio.ByteBuffer
+import java.nio.file.Files
+import java.nio.file.Paths
+
+/**
+ * @author Deividas Popelskis <deividas.popelskis@gmail.com>
+ */
+class VirtualMachine {
+
+    val memory = ByteBuffer.allocate(MEMORY_SIZE)!!
+
+    fun loadProgram(data: ByteArray) {
+        reset()
+
+        memory.position(PROGRAM_OFFSET)
+        memory.put(data)
+        memory.position(PROGRAM_OFFSET)
+    }
+
+    fun loadProgram(path: String) {
+        val data = Files.readAllBytes(Paths.get(path))
+
+        loadProgram(data)
+    }
+
+    private fun reset() {
+        memory.clear()
+    }
+
+    companion object {
+        const val MEMORY_SIZE = 0x1000
+        const val PROGRAM_OFFSET = 0x200
+    }
+
+}
