50f66e9851bee157136a0b0f069d72a817db3592
Deividas Popelskis <deividas.popelskis@gmail.com>
2017-10-12 23:03:33 +0300

    Added tests for instructions 0x3 - 0x7

--- src/test/kotlin/com/github/deividasp/kchip8/emulator/VirtualMachineTests.kt
+++ src/test/kotlin/com/github/deividasp/kchip8/emulator/VirtualMachineTests.kt
@@ -2,19 +2,20 @@ package com.github.deividasp.kchip8.emulator
 
 import com.github.deividasp.kchip8.extension.getResourcePath
 import com.github.deividasp.kchip8.extension.load
-import kotlin.test.assertEquals
 import org.junit.Test
+import kotlin.test.assertEquals
 
 /**
  * @author Deividas Popelskis <deividas.popelskis@gmail.com>
  */
 class VirtualMachineTests {
 
+    private val vm = VirtualMachine()
+
     @Test
     fun `load program from a byte array`() {
         val data = byteArrayOf(0x10, 0x20, 0x30)
 
-        val vm = VirtualMachine()
         vm.loadProgram(data)
 
         data.forEach { assertEquals(it, vm.memory.get(), "Memory value mismatch") }
@@ -25,7 +26,6 @@ class VirtualMachineTests {
         val fileName = "321"
         val expectedValues = byteArrayOf(3, 2, 1)
 
-        val vm = VirtualMachine()
         vm.loadProgram(this::class.getResourcePath(fileName))
 
         expectedValues.forEach { assertEquals(it, vm.memory.get(), "Memory value mismatch") }
@@ -35,7 +35,6 @@ class VirtualMachineTests {
     fun `load instructions`() {
         val instructions = shortArrayOf(0x1012, 0x2123, 0x3234)
 
-        val vm = VirtualMachine()
         vm.load(*instructions)
 
         instructions.forEach { assertEquals(it, vm.memory.short, "Memory value mismatch") }
--- src/test/kotlin/com/github/deividasp/kchip8/interpreter/InterpreterTests.kt
+++ src/test/kotlin/com/github/deividasp/kchip8/interpreter/InterpreterTests.kt
@@ -10,17 +10,114 @@ import kotlin.test.assertEquals
  */
 class InterpreterTests {
 
+    private val vm = VirtualMachine()
+    private val interpreter = Interpreter(vm)
+
     @Test
     fun `fetch and execute instruction 0x1`() {
-        val instruction = 0x1234.toShort()
+        val instruction = 0x1234
+        val position = instruction.and(0x0FFF)
 
-        val vm = VirtualMachine()
-        vm.load(instruction)
-
-        val interpreter = Interpreter(vm)
+        vm.load(instruction.toShort())
         interpreter.fetchAndExecuteInstruction()
 
-        assertEquals(0x234, vm.memory.position())
+        assertEquals(position, vm.memory.position(), "Incorrect memory position")
+    }
+
+    @Test
+    fun `fetch and execute instruction 0x3 (condition is unsatisfied)`() {
+        val instruction = 0x35FA
+
+        vm.load(instruction.toShort())
+        interpreter.fetchAndExecuteInstruction()
+
+        assertEquals(VirtualMachine.PROGRAM_OFFSET + 2, vm.memory.position(), "Incorrect memory position")
+    }
+
+    @Test
+    fun `fetch and execute instruction 0x3 (condition is satisfied)`() {
+        val instruction = 0x35FA
+        val registerIndex = instruction.and(0x0F00).shr(8)
+        val value = instruction.and(0x00FF)
+
+        vm.load(instruction.toShort())
+        vm.dataRegisters[registerIndex] = value
+        interpreter.fetchAndExecuteInstruction()
+
+        assertEquals(VirtualMachine.PROGRAM_OFFSET + 4, vm.memory.position(), "Incorrect memory position")
+    }
+
+    @Test
+    fun `fetch and execute instruction 0x4 (condition is unsatisfied)`() {
+        val instruction = 0x45FA
+        val registerIndex = instruction.and(0x0F00).shr(8)
+        val value = instruction.and(0x00FF)
+
+        vm.load(instruction.toShort())
+        vm.dataRegisters[registerIndex] = value
+        interpreter.fetchAndExecuteInstruction()
+
+        assertEquals(VirtualMachine.PROGRAM_OFFSET + 2, vm.memory.position(), "Incorrect memory position")
+    }
+
+    @Test
+    fun `fetch and execute instruction 0x4 (condition is satisfied)`() {
+        val instruction = 0x45FA
+
+        vm.load(instruction.toShort())
+        interpreter.fetchAndExecuteInstruction()
+
+        assertEquals(VirtualMachine.PROGRAM_OFFSET + 4, vm.memory.position(), "Incorrect memory position")
+    }
+
+    @Test
+    fun `fetch and execute instruction 0x5 (condition is unsatisfied)`() {
+        val instruction = 0x55FA
+        val firstRegisterIndex = instruction.and(0x0F00).shr(8)
+        val secondRegisterIndex = instruction.and(0x00F0).shr(4)
+
+        vm.load(instruction.toShort())
+        vm.dataRegisters[firstRegisterIndex] = 1
+        vm.dataRegisters[secondRegisterIndex] = 2
+        interpreter.fetchAndExecuteInstruction()
+
+        assertEquals(VirtualMachine.PROGRAM_OFFSET + 2, vm.memory.position(), "Incorrect memory position")
+    }
+
+    @Test
+    fun `fetch and execute instruction 0x5 (condition is satisfied)`() {
+        val instruction = 0x55FA
+
+        vm.load(instruction.toShort())
+        interpreter.fetchAndExecuteInstruction()
+
+        assertEquals(VirtualMachine.PROGRAM_OFFSET + 4, vm.memory.position(), "Incorrect memory position")
+    }
+
+    @Test
+    fun `fetch and execute instruction 0x6`() {
+        val instruction = 0x65A0
+        val registerIndex = instruction.and(0x0F00).shr(8)
+        val value = instruction.and(0x00FF)
+
+        vm.load(instruction.toShort())
+        interpreter.fetchAndExecuteInstruction()
+
+        assertEquals(value, vm.dataRegisters[registerIndex], "Data register value mismatch")
+    }
+
+    @Test
+    fun `fetch and execute instruction 0x7`() {
+        val instruction = 0x75AA
+        val registerIndex = instruction.and(0x0F00).shr(8)
+        val initialValue = 64
+        val expectedValue = instruction.and(0x00FF) + initialValue
+
+        vm.load(instruction.toShort())
+        vm.dataRegisters[registerIndex] = initialValue
+        interpreter.fetchAndExecuteInstruction()
+
+        assertEquals(expectedValue, vm.dataRegisters[registerIndex], "Data register value mismatch")
     }
 
 }
