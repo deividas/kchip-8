308532c54de0c46f1fa3621d043ad3821da332e6
Deividas Popelskis <deividas.popelskis@gmail.com>
2017-10-12 23:00:47 +0300

    Added instructions 0x3 - 0x7

--- src/main/kotlin/com/github/deividasp/kchip8/interpreter/Interpreter.kt
+++ src/main/kotlin/com/github/deividasp/kchip8/interpreter/Interpreter.kt
@@ -21,6 +21,44 @@ class Interpreter(private val virtualMachine: VirtualMachine) {
             0x1 -> { // Memory position jump
                 virtualMachine.memory.position(operand)
             }
+
+            0x3 -> { // Instruction skip (if data register == value)
+                val index = operand.and(0x0F00).shr(8)
+                val value = operand.and(0x00FF)
+
+                if (virtualMachine.dataRegisters[index] == value)
+                    virtualMachine.memory.short // skip next instruction
+            }
+
+            0x4 -> { // Instruction skip (if data register != value)
+                val index = operand.and(0x0F00).shr(8)
+                val value = operand.and(0x00FF)
+
+                if (virtualMachine.dataRegisters[index] != value)
+                    virtualMachine.memory.short // skip next instruction
+            }
+
+            0x5 -> { // Instruction skip (if [data register x] == [data register y])
+                val index1 = operand.and(0x0F00).shr(8)
+                val index2 = operand.and(0x00F0).shr(4)
+
+                if (virtualMachine.dataRegisters[index1] == virtualMachine.dataRegisters[index2])
+                    virtualMachine.memory.short // skip next instruction
+            }
+
+            0x6 -> { // Data register value assign
+                val index = operand.and(0x0F00).shr(8)
+                val value = operand.and(0x00FF)
+
+                virtualMachine.dataRegisters[index] = value
+            }
+
+            0x7 -> { // Data register value addition
+                val index = operand.and(0x0F00).shr(8)
+                val value = operand.and(0x00FF)
+
+                virtualMachine.dataRegisters[index] += value
+            }
         }
     }
 
