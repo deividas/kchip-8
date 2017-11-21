ceefdff997532765f7640dae019c1c9f527649e0
Deividas Popelskis <deividas.popelskis@gmail.com>
2017-11-21 20:41:28 +0200

    Added scheduler to virtual machine

--- src/main/kotlin/com/github/deividasp/kchip8/vm/VirtualMachine.kt
+++ src/main/kotlin/com/github/deividasp/kchip8/vm/VirtualMachine.kt
@@ -1,14 +1,23 @@
 package com.github.deividasp.kchip8.vm
 
+import com.github.deividasp.kchip8.interpreter.Interpreter
+import java.awt.Toolkit
 import java.nio.ByteBuffer
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.*
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledFuture
+import java.util.concurrent.TimeUnit
 
 /**
  * @author Deividas Popelskis <deividas.popelskis@gmail.com>
  */
 class VirtualMachine {
+
+    private var executionTask: ScheduledFuture<*>? = null
+
+    private val interpreter = Interpreter(this)
 
     val input = Input()
     val screen = Screen()
@@ -22,10 +31,26 @@ class VirtualMachine {
 
     val dataRegisters = IntArray(DATA_REGISTERS)
 
+    fun startExecution() {
+        executionTask?.cancel(true)
+
+        executionTask = Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate({
+            interpreter.fetchAndExecuteInstruction()
+
+            if (delayTimer > 0)
+                delayTimer--
+
+            if (soundTimer > 0) {
+                if (soundTimer-- == 1)
+                    Toolkit.getDefaultToolkit().beep()
+            }
+        }, EXECUTION_RATE, EXECUTION_RATE, TimeUnit.MILLISECONDS)
+    }
+
     fun loadProgram(data: ByteArray) {
         reset()
 
-        Screen.FONT_DATA.forEach { value -> memory.put(value.toByte()) }
+        Screen.FONT_DATA.forEach { memory.put(it.toByte()) }
 
         memory.position(PROGRAM_OFFSET)
         memory.put(data)
@@ -34,6 +59,9 @@ class VirtualMachine {
 
     fun loadProgram(path: String) {
         val data = Files.readAllBytes(Paths.get(path))
+
+        if (data.size > PROGRAM_SIZE_LIMIT)
+            throw Exception("Program size limit exceeded")
 
         loadProgram(data)
     }
@@ -57,6 +85,7 @@ class VirtualMachine {
         const val MEMORY_SIZE = 0x1000
         const val PROGRAM_OFFSET = 0x200
         const val EXECUTION_RATE = 1000 / 500L
+        const val PROGRAM_SIZE_LIMIT = MEMORY_SIZE - PROGRAM_OFFSET
     }
 
 }
